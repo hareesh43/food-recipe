1c106f189936910ff89187ec182c2e43cfab1161
hareesh43 <hareeshgummani@gmail.com>
2021-04-06 21:22:26 +0530

    connected to contentful

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
-export default function Home() {
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipies: res.items,
+    },
+  };
+}
+
+export default function Home({ recipies }) {
+  console.log(recipies);
   return <div>Recipes List</div>;
 }
