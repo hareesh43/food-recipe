b83febb34e512078d28d095c127c3ab6ccb1ef58
hareesh43 <hareeshgummani@gmail.com>
2021-04-09 17:39:32 +0530

    added paths to recepi details page

--- pages/recipe/[slug].js
+++ pages/recipe/[slug].js
@@ -1,3 +1,37 @@
-export default function Details() {
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  const paths = res.items.map((item) => {
+    return { params: { slug: item.fields.slug } };
+  });
+
+  return {
+    paths: paths,
+    fallback:false
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      recipies: items[0],
+    },
+  };
+}
+
+export default function Details({ recipies }) {
+  console.log(recipies);
   return <div>Recipe Details</div>;
 }
